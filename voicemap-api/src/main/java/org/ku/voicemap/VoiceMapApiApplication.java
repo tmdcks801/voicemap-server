0e2cd6e331bfa5bc6c20fcb91e345491a3bbed44
donghoony <aru0504@naver.com>
2025-09-29 22:30:26 +0900

    init: Initialize project

---
+++ voicemap-api/src/main/java/org/ku/voicemap/VoiceMapApiApplication.java
@@ -0,0 +1,12 @@
+package org.ku.voicemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceMapApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(VoiceMapApiApplication.class, args);
+    }
+}
